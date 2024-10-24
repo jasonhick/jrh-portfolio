3815b1da856aa55a81a82fcf1efae162b2b09872
Jason Hick <1746397+jasonhick@users.noreply.github.com>
2024-10-24 11:50:35 +0100

    Update to Node version and package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist/apps/frontend/browser')));
+app.use(express.static(path.join(__dirname, './dist/apps/frontend/browser')));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
